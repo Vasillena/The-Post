b94730f997e8150c464a8c2328a1a65034ef2a3f
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-10-12 22:00:03 +0300

    Add files via upload

---
+++ locales/en.ts
@@ -0,0 +1,74 @@
+export default {
+  nav: {
+    home: "Home",
+    about: "About",
+    menu: "Menu",
+    contact: "Contact",
+  },
+  hero: {
+    title: "Rhythms, cocktails and unforgettable nights at Q",
+    "title-2": "Where every night is a masterpiece!",
+    text: "Immerse yourself in a world where great beats meet the art of cocktails. Expect more than just a night out – expect an experience.",
+    button: "Save your spot for the next party",
+    countdown: "Until the next party...",
+    days: "DAYS",
+    hours: "HOURS",
+    minutes: "MINUTES",
+    seconds: "SECONDS",
+  },
+  events: {
+    title: "Upcoming Events",
+    month: "October",
+    "day-1": "tuesday",
+    "day-2": "thursday",
+    "day-3": "friday",
+    "day-4": "saturday",
+    "no-event-1": "Stay Tuned",
+    "no-event-2": "for",
+    "no-event-3": "Upcoming Events",
+  },
+  about: {
+    title: "     About us",
+    "text-1":
+      "              Welcome to Q – where cocktails and art come together. Q is not just a cocktail bar – it’s a destination of flavors and creativity. We created this unique space to offer our guests an experience that blends the sophistication of cocktails with the magic of music.",
+    "text-2":
+      "  Our team doesn’t just craft cocktails; we create parties that become stories to tell. With carefully selected ingredients and innovative techniques, every drink at Q is a unique masterpiece.",
+    "text-3":
+      "         Join us and discover the magic of Q – where art and pleasure meet in perfect harmony. We look forward to welcoming you!",
+  },
+  cocktails: {
+    title: "Cocktail Journeys",
+    "text-1":
+      "            At Q, we believe every cocktail should be an experience. Our masterfully crafted cocktails blend art, innovation, and attention to detail to deliver unique flavors that will surprise and inspire you. ",
+    "text-2":
+      "Explore our diverse range of signature cocktails designed to awaken your senses and take you on an unforgettable journey. Share your preferences with us, and we'll create something special just for you.",
+    "text-3":
+      "          At Q, cocktails are not just drinks—they are a manifestation of passion and creativity. Come and discover your new favorite cocktail.",
+    button: "Discover more",
+  },
+  food: {
+    title: "Gourmet Delights",
+    "text-1":
+      "At Q, it’s not just the cocktails that are top-notch—our master chefs create irresistible dishes inspired by the global culinary scene, using only the freshest and highest-quality ingredients. ",
+    "text-2":
+      "Each day, you’ll find our daily specials prepared with care and attention to detail, enhancing your experience at Q. Enjoy a flavor adventure that perfectly complements our cocktails and creates an unforgettable atmosphere in the heart of Kapana.",
+  },
+  contacts: {
+    "text-1": "Contacts ",
+    "text-2": "Address",
+    "text-2-1": "Bratq Pulievi 1,",
+    "text-2-2": "Plovdiv, Bulgaria 4000",
+    "text-3": "Working hours",
+    "text-3-1": "Monday - Sunday 9:00 - 3:00",
+    "text-4": "Follow us",
+  },
+  menu: {
+    "text-1": "Hot Drinks",
+    "text-2": "Cold Drinks",
+    "text-3": "Soft Drinks",
+    "text-4": "Mixed Drinks",
+    "text-5": "Wine",
+    "text-6": "Alcohol",
+    "text-7": "Beer",
+  },
+} as const;
